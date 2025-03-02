c01700e216482d0deac58fa74405be7e4eb78907
Saiful Islam sohan <saifulislam.cse.pust@gmail.com>
2025-03-02 14:37:18 +0600

    Implemented Role-Based Access Control (RBAC) and Product Management

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,14 +1,56 @@
 
 import './App.css'
-
+import { BrowserRouter as Router, Routes, Route, Navigate } from "react-router-dom";
+import Login from './pages/auth/Login';
+import Register from './pages/auth/Register';
+import Dashboard from './pages/Dashboard';
+import ProtectedRoute from './pages/ProtectedRoute';
+import Navbar from './components/Navbar';
+import AdminPanel from './pages/AdminPanel';
 function App() {
 
 
   return (
     <>
-      <h1 class="text-3xl font-bold underline">
-        Hello world!
-      </h1>
+      {/* <Router>
+        <Navbar />
+        <Routes>
+          <Route path="/login" element={<Login />} />
+          <Route path="/register" element={<Register />} />
+          <Route
+            path="/dashboard"
+            element={
+              <ProtectedRoute>
+                <Dashboard />
+              </ProtectedRoute>
+            }
+          />
+          <Route path="/" element={<Navigate to="/dashboard" />} />
+        </Routes>
+      </Router> */}
+
+      <Router>
+        <Routes>
+          <Route path="/login" element={<Login />} />
+          <Route path="/register" element={<Register />} />
+          <Route
+            path="/dashboard"
+            element={
+              <ProtectedRoute>
+                <Dashboard />
+              </ProtectedRoute>
+            }
+          />
+          <Route
+            path="/admin"
+            element={
+              <ProtectedRoute requiredRole="admin">
+                <AdminPanel />
+              </ProtectedRoute>
+            }
+          />
+        </Routes>
+      </Router>
     </>
   )
 }
